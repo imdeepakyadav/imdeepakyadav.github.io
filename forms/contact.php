e1db33b6d2920c677a41eb2777e6ac82f4606312
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2022-10-12 12:55:12 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -17,15 +17,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
+    'host' => 'imdeepakyadav.me',
+    'username' => 'admin',
     'password' => 'pass',
     'port' => '587'
   );
-  */
-
+  
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
