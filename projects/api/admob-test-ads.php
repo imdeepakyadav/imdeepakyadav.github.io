7286bfed7e5e276171e553995242876b8bc67e13
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2023-04-19 12:28:51 +0530

    Create admob-test-ads.php

---
+++ projects/api/admob-test-ads.php
@@ -0,0 +1,61 @@
+{
+  "ad_ids": [
+    {
+      "ad_network": "Admob",
+      "unit_type": "Banner",
+      "placement_id": "/21753324030,22723853531/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Banner",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Interstitial",
+      "placement_id": "/21753324030,22723853531/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Interstitial",
+      "priority": "1",
+      "counter": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "Native",
+      "placement_id": "/21753324030,22723853531/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Native",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "RewardAd",
+      "placement_id": "/21753324030,22723853531/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_Rewarded",
+      "priority": "1"
+    },
+    {
+      "ad_network": "Admob",
+      "unit_type": "AppOpenAd",
+      "placement_id": "/21753324030,22723853531/com.freelivevideocalling.girllivevideochat.pushpalivetalk.videorandomchat_AppOpen",
+      "priority": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Banner",
+      "placement_id": "1bd61e010479c716",
+      "priority": "0"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Interstitial",
+      "placement_id": "197f5ba608c27d44",
+      "priority": "0",
+      "counter": "1"
+    },
+    {
+      "ad_network": "AppLovin",
+      "unit_type": "Native",
+      "placement_id": "697c0a91e00756ca",
+      "priority": "0"
+    }
+  ],
+  "info": [
+    {
+      "type": "random_call_video",
+      "count": 50,
+      "is_show": "yes"
+    }
+  ]
+}
